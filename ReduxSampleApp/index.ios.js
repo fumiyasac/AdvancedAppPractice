0753307151c1e5fe3713692cad6be7ffe546c59d
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2017-02-22 14:48:25 +0900

    [WIP] make app.js file

---
+++ ReduxSampleApp/index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Reduxを使用したアプリケーションの作成サンプル
+ */
+
+//アプケーションの描画に必要なものをインポートする
+import { AppRegistry } from 'react-native';
+import App from './src/app';
+
+//アプケーションの描画を行う
+AppRegistry.registerComponent('ReduxSampleApp', () => App);
---
+++ ReduxSampleApp/src/app.js
@@ -0,0 +1,34 @@
+/**
+ * アプリケーション構築用のファイル
+ */
+
+//ReactNative用のインポート宣言
+import React from 'react';
+import { View } from 'react-native';
+
+//React-Reduxのインポート宣言
+// → ProviderタグでラップすることでReactコンポーネント内でStoreにアクセスできるようにする
+// (参考1) React+Redux入門
+// http://qiita.com/erukiti/items/e16aa13ad81d5938374e
+// (参考2) React-Redux をわかりやすく解説しつつ実践的に一部実装してみる
+// http://ma3tk.hateblo.jp/entry/2016/06/20/182232
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+//reducerのインポート宣言
+// → ざっくり言えば状態変化を起こすための具体的な処理の寄せ集め
+// (参考) Redux基礎：Reducer編
+// http://qiita.com/yasuhiro-okada-aktsk/items/9d9025cb58ffba35f864
+import reducers from './reducers';
+
+//アプリの画面の組み立て
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View />
+    </Provider>
+  );
+};
+
+//アプリの画面本体となるこのファイルのエクスポート宣言
+export default App;
