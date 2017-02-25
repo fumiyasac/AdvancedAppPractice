e4ed6cbe4f5501c9b3eb0e7c5d3dc8a889e7cb6b
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2017-02-26 00:39:31 +0900

    [WIP] Definite actions.

---
+++ ReduxEmployeeApp/src/actions/index.js
@@ -0,0 +1,9 @@
+/**
+ * アクションの一覧のインポート宣言
+ * → AuthActions：認証結果を格納するステートを更新するためのアクション
+ * → EmployeeActions：従業員データに関する結果を格納するステートを更新するためのアクション
+ */
+
+//それぞれの機能ごとのアクション一覧のインポート宣言
+export * from './AuthActions';
+export * from './EmployeeActions';
---
+++ ReduxEmployeeApp/src/actions/types.js
@@ -0,0 +1,17 @@
+/**
+ * アクションのタイプを定義した定数
+ * → 該当の定数で定義されたアクションを実行することでステートを更新する
+ */
+
+//認証部分に関するアクションの定義
+export const EMAIL_CHANGED = 'email_changed';
+export const PASSWORD_CHANGED = 'password_changed';
+export const LOGIN_USER_SUCCESS = 'login_user_success';
+export const LOGIN_USER_FAIL = 'login_user_fail';
+export const LOGIN_USER = 'login_user';
+
+//従業員の一覧・追加・変更に関するアクションの定義
+export const EMPLOYEE_UPDATE = 'employee_update';
+export const EMPLOYEE_CREATE = 'employee_create';
+export const EMPLOYEES_FETCH_SUCCESS = 'employees_fetch_success';
+export const EMPLOYEE_SAVE_SUCCESS = 'employee_save_success';
